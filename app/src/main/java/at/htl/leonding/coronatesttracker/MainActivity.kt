3c4b5d254a3193c2f8c53e1828e87b3072f98a71
David_Hieselmayr <unbekannt.ghost@gmail.com>
2021-12-13 09:43:37 +0100

    update ids

--- app/src/main/java/at/htl/leonding/coronatesttracker/MainActivity.kt
+++ app/src/main/java/at/htl/leonding/coronatesttracker/MainActivity.kt
@@ -17,67 +17,8 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-/*
-*
-* class MainActivity : AppCompatActivity(), DatePickerDialog.OnDateSetListener,
-    TimePickerDialog.OnTimeSetListener {
-* */
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // pickDate()
     }
-
-
-/*
-
-    var day = 0
-    var month = 0
-    var year = 0
-    var hour = 0
-    var minute = 0
-
-    var savedDay = 0
-    var savedMonth = 0
-    var savedYear = 0
-    var savedHour = 0
-    var savedMinute = 0
-
-    private fun pickDate() {
-        btn_timePicker.setOnClickListener {
-            getDateTimeCalender()
-
-            DatePickerDialog(this, this, year, month, day).show()
-        }
-    }
-
-    private fun getDateTimeCalender() {
-        val cal = Calendar.getInstance()
-        day = cal.get(Calendar.DAY_OF_MONTH)
-        month = cal.get(Calendar.MONTH)
-        year = cal.get(Calendar.YEAR)
-        hour = cal.get(Calendar.HOUR)
-        minute = cal.get(Calendar.MINUTE)
-    }
-
-    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        savedDay = dayOfMonth
-        savedMonth = month
-        savedYear = year
-
-        getDateTimeCalender()
-        TimePickerDialog(this, this, hour, minute, true).show()
-    }
-
-    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
-        savedHour = hourOfDay
-        savedMinute = minute
-
-        tv_textTime.text =
-            "$savedDay-$savedMonth-$savedYear\n Hour: $savedHour Minute: $savedMinute"
-    }
-*/
-
 }
